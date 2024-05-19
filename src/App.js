f0651c0b4988913ca343654f6654a89a07c1bd38
Kumar Harsh <kharsh560@gmail.com>
2024-05-19 15:42:02 +0530

    initial commit after making some files
    initial commit after making some files; Needed to do so bcoz I had changed the name of the master folder of the repo which contained the file containing all the codes.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className=" bg-sky-800 h-screen flex items-center justify-center font-medium">
+      <h1 className=" font-bold text-7xl text-lime-300">LacasaDePapel</h1>
+    </div>
+  );
+}
+
+export default App;
